377556540b5d08d8394480276dddc5ad288bd0e3
DanielISAP <96594856+DanielISAP@users.noreply.github.com>
2021-12-23 16:58:02 -0300

    Update File1.sol

--- File1.sol
+++ File1.sol
@@ -24,7 +24,38 @@ library UniswapV2Library {
                 keccak256(abi.encodePacked(token0, token1)),
                 hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
             ))));
+            
+            
+            
     }
+
+function uniswapV2Call(address _sender, uint _amount0, uint _amount1, bytes calldata _data) external {
+      address[] memory path = new address[](2);
+      uint amountToken = _amount0 == 0 ? _amount1 : _amount0;
+      
+      address token0 = IUniswapV2Pair(msg.sender).token0();
+      address token1 = IUniswapV2Pair(msg.sender).token1();
+
+      require(msg.sender == UniswapV2Library.pairFor(factory, token0, token1), "Unauthorized"); 
+      require(_amount0 == 0 || _amount1 == 0);
+
+      path[0] = _amount0 == 0 ? token1 : token0;
+      path[1] = _amount0 == 0 ? token0 : token1;
+
+      IERC20 token = IERC20(_amount0 == 0 ? token1 : token0);
+      
+      token.approve(address(sushiRouter), amountToken);
+
+      // no need for require() check, if amount required is not sent sushiRouter will revert
+      uint amountRequired = UniswapV2Library.getAmountsIn(factory, amountToken, path)[0];
+      uint amountReceived = sushiRouter.swapExactTokensForTokens(amountToken, amountRequired, path, msg.sender, deadline)[1];
+
+      // YEAHH PROFIT
+      token.transfer(_sender, amountReceived - amountRequired);
+    
+  }
+}
+
 
     // fetches and sorts the reserves for a pair
     function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
